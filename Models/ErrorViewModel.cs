98ebb83ec3b2652bf367c634c5cd839ae0b9fcf0
Gustavo Henrique da Silva <gustavohs2004@gmail.com>
2021-08-22 13:51:29 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetMvcMicrosoftDocs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
