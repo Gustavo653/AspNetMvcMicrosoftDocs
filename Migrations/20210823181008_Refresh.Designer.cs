35225ecb6fff26f8a74b758789533d729aefd122
Gustavo Henrique da Silva <gustavohs2004@gmail.com>
2021-08-23 15:12:09 -0300

    Validacao de campos

--- Migrations/20210823181008_Refresh.Designer.cs
+++ Migrations/20210823181008_Refresh.Designer.cs
@@ -4,14 +4,16 @@ using AspNetMvcMicrosoftDocs.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AspNetMvcMicrosoftDocs.Migrations
 {
     [DbContext(typeof(AspNetMvcMicrosoftDocsContext))]
-    partial class AspNetMvcMicrosoftDocsContextModelSnapshot : ModelSnapshot
+    [Migration("20210823181008_Refresh")]
+    partial class Refresh
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
