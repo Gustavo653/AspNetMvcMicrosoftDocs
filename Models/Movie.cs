d78191e33c8c55c3c20f641c000b168f2042813a
Gustavo Henrique da Silva <gustavohs2004@gmail.com>
2021-08-23 14:50:34 -0300

    Pesquisa personalizada

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,10 +11,11 @@ namespace AspNetMvcMicrosoftDocs.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-
+        [Display(Name = "Release date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
